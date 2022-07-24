5157e86687ac8b970933738758845b57e7523dfb
wwd123123 <1245436962@qq.com>
2022-07-24 20:48:00 +0800

    项目搭建完成

---
+++ icc-menber/src/main/java/com/example/iccmenber/demo01/Person.java
@@ -0,0 +1,20 @@
+package com.example.iccmenber.demo01;
+
+import lombok.Data;
+
+@Data//可省略setter、getter
+public class Person {
+
+    // 参数名一定要一致
+    Integer PersonId;
+    String Pname;
+    int Page;
+    String Message;//增加message属性，保存请求的URL
+
+    public Person(Integer personId, String pname, int page) {
+        PersonId = personId;
+        Pname = pname;
+        Page = page;
+    }
+
+}
---
+++ icc-menber/src/main/java/com/example/iccmenber/demo01/PersonController.java
@@ -0,0 +1,38 @@
+package com.example.iccmenber.demo01;
+
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Maps;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/menber/provider")
+public class PersonController {
+
+    //实验：模拟返回查询数据
+    @RequestMapping(value = "/person/{personId}", method = RequestMethod.GET)
+    public String findName(@PathVariable("personId") Integer personId, HttpServletRequest request){
+        Person person = new Person(personId, "TOM", 23);
+        person.setMessage(request.getRequestURL().toString()); //保存请求的URL
+        Map<String, String> paramterMap = Maps.newHashMap();
+        paramterMap.put("PersonId", person.getPersonId().toString());
+        paramterMap.put("Pname", person.getPname());
+        paramterMap.put("Page", Integer.toString(person.getPage()));
+        paramterMap.put("Message", person.getMessage());
+        String str = JSON.toJSONString(paramterMap);
+        System.out.println("provider：访问person成功！" + str);
+        return str;
+    }
+
+    //实验5：feign服务端指定请求内容类型           consume是：指定请求内容类型；Produces是：指定返回内容类型。
+    @RequestMapping(value = "/person/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String createPerson(@RequestBody Person person){
+        System.out.println("请求内容：" + person);
+        return "success, person id:"+person.PersonId;
+    }
+
+}
